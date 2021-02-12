45c49bf7f1aee3f69c64e3fd3028597ba38be5b3
Phil Lachance <38385593+menipe@users.noreply.github.com>
2021-02-12 11:08:58 -0500

    Updated path

--- moodle/setup.sh
+++ moodle/setup.sh
@@ -1,4 +1,4 @@
 curl -sSL https://raw.githubusercontent.com/menipe/docker/main/moodle/.env > .env
-curl -sSL https://raw.githubusercontent.com/menipe/docker/main/docker-compose.yml > docker-compose.yml
+curl -sSL https://raw.githubusercontent.com/menipe/docker/main/moodle/docker-compose.yml > docker-compose.yml
 
 docker-compose up -d
